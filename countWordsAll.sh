5cad864765b7d895de3b9d0be499388a12e21141
Fei Yan <fei.yan@nokia-sbell.com>
2019-10-03 11:15:30 +0800

    site updated at 2019-10-03_11:15:29

--- countWordsAll.sh
+++ countWordsAll.sh
@@ -2,9 +2,10 @@
 total=0
 for fname in $(find _deploy/post -type f -a -name "*.html"); do
     count=$(cat "${fname}" | sed -n  "s/.*本文有\s*\([0-9]\+\).*/\1/gp")
+    postName=$(echo ${fname} | sed -n "s/.*post\/\(.*\)\/index.html/\1/gp" | sed -n "s/\//-/gp")
     if [ -n "$count" ]; then
         total=$(($total+$count))
-        echo "$fname | $count"
+        echo "$postName | $count"
     else
         echo "no count for ", $fname
     fi
